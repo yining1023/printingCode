4c39a3c0841362001c25a10c889060c5245190e9
Yining <ys2189@nyu.edu>
2015-12-12 17:21:05 -0500

    tweaked a lot of things, added curved lines for circles

--- final-sincos/sketch.js
+++ final-sincos/sketch.js
@@ -1,12 +1,12 @@
 var r = new Rune({
   container: "#canvas",
-  width: 2400,
-  height: 3600,
+  width: 2600,
+  height: 4000,
   // debug: true
 });
 
+r.rect(0, 0, r.width, r.height).fill(30).stroke(false);
 // r.rect(0, 0, r.width, r.height).fill("#ffffff").stroke(false);
-r.rect(0, 0, r.width, r.height).fill("#ffffff").stroke(false);
 
 var grid = r.grid({
   x: 0,
@@ -22,8 +22,18 @@ var fullMessage;
 var color1 = new Rune.Color(0, 174, 239);
 var color = new Rune.Color(244, 154, 193);
 var radius = 480;
+var radius2 = 240;
+
 var points = 240;
 var spacing = 360/points;
+
+var col = 0.15;
+
+var floor = 0;
+var miss = 0;
+var haha = 0;
+var doing = 0;
+var home = 0;
 
 $.getJSON('fullMessage.json')
    .done(function (data) {
@@ -36,6 +46,13 @@ $.getJSON('fullMessage.json')
     // {
     //   for(var i = 1; i < 101; i++){
       for(var i = 0; i < 10000; i++){
+        if(i==9999){
+          console.log(miss);
+          console.log(home);
+          console.log(haha);
+          console.log(floor);
+          console.log(doing);
+        }
         var name = fullMessage[i].author;
         var text = fullMessage[i].text;
         var time = fullMessage[i].timestamp;
@@ -50,7 +67,7 @@ $.getJSON('fullMessage.json')
         // Seconds part from the timestamp
         var seconds = "0" + date.getSeconds();
 
-          console.log(hours);
+          // console.log(hours);
         
         // console.log(hours);
         var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
@@ -59,46 +76,60 @@ $.getJSON('fullMessage.json')
         // console.log(minutes);
 
         if(name == "Yining  Shi"){
-          if(text.indexOf("I miss you") > -1 || text.indexOf("i miss you") > -1 || text.indexOf("i missed you") > -1 || text.indexOf("I missed you") > -1){
-            var color = new Rune.Color("#ff0000", 0.2);
+          if(text.toLowerCase().indexOf("i miss you") > -1 ||
+            text.toLowerCase().indexOf("i missed you") > -1) {
+
+            var color = new Rune.Color("#ed145b", 0.4);
+            var colorLine = new Rune.Color("#ed145b", col);
+            var xWord = 1100;
             //red color
-            // console.log(name);
-            // console.log(text);
-            // console.log(time);
+            miss++;
           }
-          else if(text.indexOf("me too") > -1 || text.indexOf("Me too") > -1){
-            var color = new Rune.Color("#af3aeb", 0.2);
+          else if(text.toLowerCase().indexOf("home") > -1){
+            var color = new Rune.Color("#3568db", 0.4);
+            var colorLine = new Rune.Color("#3568db", col);
+            var xWord = 1200;
             // console.log(text);
+            home++;
           }
-          else if(text.indexOf("haha") > -1 || text.indexOf("Haha") > -1){
-            var color = new Rune.Color("#62C462", 0.2);
+          else if(text.toLowerCase().indexOf("haha") > -1){
+            // var color = new Rune.Color("#36aa09", 0.3);
+            var color = new Rune.Color("#fff568", 0.4);
+            var colorLine = new Rune.Color("#fff568", col);
+            var xWord = 1300;
             // console.log(text);
+            haha++;
           }
-          else if(text.indexOf("floor") > -1 || text.indexOf("Floor") > -1){
-            var color = new Rune.Color("#fe9001", 0.2);
+          else if(text.toLowerCase().indexOf("floor") > -1){
+            var color = new Rune.Color("#fe9001", 0.4);
+            var colorLine = new Rune.Color("#fe9001", col);
+            var xWord = 1400;
             // console.log(text);
+            floor++;
           }
-          else if(text.indexOf("data") > -1 || text.indexOf("Data") > -1){
-            var color = new Rune.Color("#1F3A93", 0.2);
-            // console.log(text);
-            // console.log(name);
-            // console.log(text);
-            // console.log(time);
+          else if(text.toLowerCase().indexOf("what are you doing") > -1 || text.toLowerCase().indexOf("whatcha doin") > -1){
+            var color = new Rune.Color("#82ca9c", 0.4);
+            var colorLine = new Rune.Color("#82ca9c", col);
+            var xWord = 1500;
+            doing++;
           }
           else{
             var color = new Rune.Color(244, 154, 193, 0.2);
+            var colorLine = new Rune.Color(244, 154, 193, col);
+            var xWord = 0;
             // console.log(text);
           }
 
           if(hours >= 0 && hours <= 11){
             radius = 480;
+            randomPos = Rune.random(-90,90);
           }
           else if(hours >= 12 && hours <= 23){
-            radius = 720;
+            radius = 960;
             hours -= 12; 
+            randomPos = Rune.random(-110,110);
           }
-
-          randomPos = Rune.random(-70,70);
+          
           radius += randomPos;
           var size = 20 + textLength/10;
           var x = r.width/2+Math.cos(Rune.radians(2*hours*10 * spacing - 90 + minutes*30/60)) * radius;
@@ -110,54 +141,110 @@ $.getJSON('fullMessage.json')
           // console.log(Rune.radians(hours*10 * spacing - 90));
 
           var tree2 = r.circle(x, y, size)
-          .fill(color)
-          .stroke(false);
+            .fill(color)
+            .stroke(false);
+
+          if(xWord !== 0){
+            // var line = r.line(x, y, xWord, 3800)
+            //   .stroke(color);
+            var path = r.path(x, y)
+              .curveTo(xWord-x, 1900-y, xWord-x, 3800-y)
+              .stroke(colorLine)
+              .fill(false);
+          }
         }
 
-        // else if(name == "Sagar Mohite"){
-        //   if(text.indexOf("I miss you") > -1 || text.indexOf("i miss you") > -1 || text.indexOf("i missed you") > -1 || text.indexOf("I missed you") > -1){
-        //     var color2 = new Rune.Color("#ff0000");
-        //     //red color
-        //     // console.log(name);
-        //     // console.log(text);
-        //     // console.log(time);
-        //     // console.log(i);
-        //     // console.log(j);
-        //   }
-        //   else if(text.indexOf("me too") > -1 || text.indexOf("Me too") > -1){
-        //     var color2 = new Rune.Color("#af3aeb");
-        //     //purple
-        //     // console.log(text);
-        //   }
-        //   else if(text.indexOf("haha") > -1 || text.indexOf("Haha") > -1){
-        //     var color2 = new Rune.Color("#62C462");
-        //     // console.log(text);
-        //     //green
-        //   }
-        //   else if(text.indexOf("floor") > -1 || text.indexOf("Floor") > -1){
-        //     var color2 = new Rune.Color("#fe9001");
-        //     // console.log(text);
-        //     //orange
-        //   }
-        //   else if(text.indexOf("data") > -1 || text.indexOf("Data") > -1){
-        //     var color2 = new Rune.Color("#1F3A93");
-        //     // console.log(text);
-        //     // console.log(name);
-        //     // console.log(text);
-        //     // console.log(time);
-        //   }
-        //   else{
-        //     var color2 = new Rune.Color("#5BC0DE");
-        //     //light blue
-        //   }
+        else if(name == "Sagar Mohite"){
+          if(text.toLowerCase().indexOf("i miss you") > -1 ||
+            text.toLowerCase().indexOf("i missed you") > -1) {
 
-        //   var x = Math.cos(Rune.radians(hours*10 * spacing)) * radius;
-        //   var y = Math.sin(Rune.radians(hours*10 * spacing)) * radius;
+            var color2 = new Rune.Color("#ed145b", 0.4);
+            var colorLine2 = new Rune.Color("#ed145b", col);
+            var xWord2 = 1100;
+            miss++;
+            //red color
+          }
+          else if(text.toLowerCase().indexOf("home") > -1){
+            var color2 = new Rune.Color("#3568db", 0.4);
+            var colorLine2 = new Rune.Color("#3568db", col);
+            var xWord2 = 1200;
+            home++;
+            //purple
+            // console.log(text);
+          }
+          else if(text.toLowerCase().indexOf("haha") > -1){
+            // var color2 = new Rune.Color("#1cbb90", 0.3);
+            var color2 = new Rune.Color("#fff568", 0.4);
+            var colorLine2 = new Rune.Color("#fff568", col);
+            var xWord2 = 1300;
+            haha++;
+            // console.log(text);
+            //green
+          }
+          else if(text.toLowerCase().indexOf("floor") > -1){
+            var color2 = new Rune.Color("#fe9001", 0.4);
+            var colorLine2 = new Rune.Color("#fe9001", col);
+            var xWord2 = 1400;
+            floor++;
+            // console.log(text);
+            //orange
+          }
+          else if(text.toLowerCase().indexOf("what are you doing") > -1 || text.toLowerCase().indexOf("whatcha doin") > -1){
+            var color2 = new Rune.Color("#82ca9c", 0.2);
+            var colorLine2 = new Rune.Color("#82ca9c", col);
+            var xWord2 = 1500;
+            doing++;
+            // console.log(text);
+            // console.log(name);
+            // console.log(text);
+            // console.log(time);
+          }
+          else{
+            // var color2 = new Rune.Color("#5BC0DE", 0.2);
+            var color2 = new Rune.Color("#6dcff6", 0.3);
+            var colorLine2 = new Rune.Color("#6dcff6", col);
+            var xWord2 = 0;
+            //light blue
+          }
 
-        //   var tree2 = r.triangle(x, y - size/2, x - Math.sqrt(2)/2*(x), y + size*3/4, x + Math.sqrt(2)/2*(x), y + size*3/4)
-        //   .fill(color2)
-        //   .stroke(false);
-        // }
+          if(hours >= 0 && hours <= 11){
+            radius2 = 240;
+            randomPos = Rune.random(-90,90);
+          }
+          else if(hours >= 12 && hours <= 23){
+            radius2 = 720;
+            hours -= 12;
+            randomPos = Rune.random(-110, 110); 
+          }
+
+          
+          radius2 += randomPos;
+          var size2 = 20 + textLength/10;
+          // var size2 = 30;
+          var x2 = r.width/2+Math.cos(Rune.radians(2*hours*10 * spacing - 90 + minutes*30/60)) * radius2;
+          var y2 = -400+r.height/2+Math.sin(Rune.radians(2*hours*10 * spacing - 90 + minutes*30/60)) * radius2;
+
+          // var tree2 = r.triangle(x2, y2 - size2, 
+          //   x2 - Math.sqrt(2)/2*(size2), y2 + size2/2, 
+          //   x2 + Math.sqrt(2)/2*(size2), y2 + size2/2)
+          
+          var tree2 = r.circle(x2, y2, size2)
+          .fill(color2)
+          .stroke(false);
+
+          // var color3 = color2
+          // var color4 = color3.darken(0.5);
+          if(xWord2 !== 0){
+            var path = r.path(x2, y2)
+              .curveTo(xWord2-x2, 1900-y2, xWord2-x2, 3800-y2)
+              .stroke(colorLine2)
+              .fill(false);
+          }
+          // .rotate(Rune.random(0,360));
+          // console.log(tree2);
+          // console.log(y2 - size2);
+          // console.log(y2 + size2/2);
+        }
       }
     r.draw();
    });
